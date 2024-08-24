5fe7623913051c89716758147638681ce8262e3a
vorobyevd <sparrow2804@gmail.com>
2024-08-24 13:24:10 +0300

    Add regulator.py file and template for PI_Regulator class inside

---
+++ motor_control_module/regulator.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+"""
+    File regulator.py  contains regulators of different types
+
+"""
+ 
+ # ****************************** PI-regulator class ********************************
+
+class PI_Regulator(object):
+    """
+    Class implements PI-regulator functionality
+
+    """
+    def __init__(self) -> None:
+        pass
